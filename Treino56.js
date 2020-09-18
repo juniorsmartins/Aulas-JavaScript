3fc85808580ea92599f790a7ece2842d7df455a8
Junior Martins <juniorsoaresmartins@gmail.com>
2020-09-18 17:44:44 -0400

    Aula Js
    Aula Js

--- Treino56.js
+++ Treino56.js
@@ -1,19 +1,20 @@
 var data_Atual = 0;
+var data_Copa = 0;
 
 function pula_Linha()
 {
   document.write('<br/><br/>');
 }
 
-function mostra(frase)
+function mostra(let frase)
 {
   document.write(frase + '<br/>');
 }
 
 function primeiro_Evento()
 {
-  var ano_Evento = parseInt(window.prompt('Entrada de Dados', 'Qual o ano da 1ª Copa do Mundo? '));
-  return (ano_Evento)
+  let ano_Evento = parseInt(window.prompt('Entrada de Dados', 'Qual o ano da 1ª Copa do Mundo? '));
+  return (ano_Evento);
 }
 
 pula_Linha();
@@ -21,12 +22,12 @@ mostra('**********************');
 mostra('****** Aula de Js ******');
 mostra('**********************');
 pula_Linha();
-var data_Copa = primeiro_Evento();
-mostra('A 1ª Copa do Mundo foi em: ', data_Copa);
+data_Copa = primeiro_Evento();
+mostra('A 1ª Copa do Mundo foi em: ' + data_Copa);
 
 do
 {
   data_Atual = (data_Copa + 4);
-  mostra('Haverá Copa do Mundo em: ', data_Atual);
+  mostra('Haverá Copa do Mundo em: ' + data_Atual);
 } while (data_Atual <= 2030);
 

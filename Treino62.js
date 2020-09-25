f4feeb3651a11b82158b1600aa5fe3427505c81a
Junior Martins <juniorsoaresmartins@gmail.com>
2020-09-25 10:05:06 -0400

    Aula Js
    Aula Js

---
+++ Treino62.js
@@ -0,0 +1,15 @@
+console.log("");
+console.log("JavaScript - Aula sobre Objetos");
+console.log("");
+
+var meu_Carro = 
+{
+  modelo: 'Porsche', 
+  ano: 1970,
+  cor: 'Preto'
+};
+
+console.log(meu_Carro);
+console.log(meu_Carro.modelo);
+console.log(meu_Carro.ano);
+console.log(meu_Carro['cor']);

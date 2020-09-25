181559d0a21b6e0fad303c574666b327f95f5156
Junior Martins <juniorsoaresmartins@gmail.com>
2020-09-25 17:01:51 -0400

    Aula Js
    Aula Js

---
+++ Treino63.js
@@ -0,0 +1,11 @@
+var calculo;
+console.log(calculo = Math.round(.6));
+console.log(calculo = Math.ceil(.6));
+console.log(calculo = Math.floor(1.6));
+
+var nome = "Mário", sobrenome = "Oliveira", profissao = "Dentista", nome2 = "Eva";
+
+console.log(`O nome é : ${nome}`);
+
+console.log(nome.length);
+console.log(nome2.length);

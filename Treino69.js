0b39b5a10b82e52c4d9d47c2b3f8e7619a81873f
Junior Martins <juniorsoaresmartins@gmail.com>
2020-10-02 15:29:16 -0400

    Aula Js
    Aula Js

---
+++ Treino69.js
@@ -0,0 +1,17 @@
+var numero = 1;
+while(numero <= 10)
+{
+  document.write(numero + "<br/>");
+  numero+=1;
+};
+
+var numero2;
+for (numero2 = 10; numero2 >= 1; numero2--)
+{
+  document.write(numero2 + "<br/>");
+};
+
+for  (let numero3 = 1; numero3 <= 10; numero3++)
+{
+  document.write(numero3);
+};

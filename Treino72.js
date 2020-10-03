9b2fd4ff7135d176c204175168ebf217821437cd
Junior Martins <juniorsoaresmartins@gmail.com>
2020-10-03 18:39:27 -0400

    Aula C
    Aula C

---
+++ Treino72.js
@@ -0,0 +1,12 @@
+window.console.log
+(
+  window.document.getElementById("Titulo").innerHTML
+);
+
+window.console.log
+(
+  window.screen.width,
+  window.screen.height,
+  window.navigator.appVersion,
+  window.navigator.geolocation  
+);

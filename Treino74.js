e1d8fab278be093f12788affecf7782ac46262a6
Junior Martins <juniorsoaresmartins@gmail.com>
2020-10-05 14:48:06 -0400

    Aula Js
    Aula Js

---
+++ Treino74.js
@@ -0,0 +1,15 @@
+var frutas, variavel2, variavel3;
+
+frutas = ["Banana", "Melão", "Limão", "Uva"];
+variavel2 = frutas.length;
+variavel3 = "<ul>";
+
+for (let contador = 0; contador <= variavel2; contador+=1)
+{
+  variavel3 += ("<li>" + frutas[contador] + "</li>");
+};
+
+window.document.write("<br/> Tamanho: ", variavel2);
+window.document.write("<br/> Resultado: ", variavel3);
+window.document.write("<br/>" + frutas + "<br/>");
+

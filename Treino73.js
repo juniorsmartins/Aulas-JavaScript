07873cd4ad28e4aabc702c8439774d047d765dc7
Junior Martins <juniorsoaresmartins@gmail.com>
2020-10-05 11:11:44 -0400

    Aula Js
    Aula Js

---
+++ Treino73.js
@@ -0,0 +1,26 @@
+// Declaração de Variáveis
+
+var a;
+var b = 10;
+var c, d, e;
+d = null;
+var f = 2, g = 3, h = 5;
+var nome = "Mário";
+
+window.document.write(a, "<br/>", b, "<br/>", c, "<br/>", d, "<br/>", e, "<br/>", f, "<br/>", g, "<br/>", h, "<br/>", nome, "<br/>");
+
+for (let i = 1; i < 6; i++)
+{
+  window.document.write(nome);
+}
+window.document.write("<br/>");
+for (let t = 1, j = 3; t < j; t++) 
+{
+  window.document.write("<br/>", t);
+};
+window.document.write("<br/>");
+for (let y = 1, k = 8; y < 6; y++, k--)
+{
+  window.document.write(y, " = ", k, "<br/>");
+};
+
